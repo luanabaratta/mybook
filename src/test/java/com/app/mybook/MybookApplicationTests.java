9d251da0db2ab20eb729f889e9ac32884853683d
Luana <luanabaratta.technology@gmail.com>
2025-09-16 22:48:31 -0300

    folders structure

---
+++ src/test/java/com/app/mybook/MybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
