56b1063197c7cdbaf4a2cb3d7ef99a513ed17bd6
Luana <luanabaratta.technology@gmail.com>
2025-09-17 01:22:10 -0300

    findByLogin method

--- src/main/java/com/app/mybook/repository/UserRepository.java
+++ src/main/java/com/app/mybook/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.app.mybook.repository;
 
 import com.app.mybook.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
 }
