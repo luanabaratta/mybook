e4ccc1284600d6447808db9a323897828702a0a4
Luana <luanabaratta.technology@gmail.com>
2025-09-17 00:05:38 -0300

    properties

--- src/main/java/com/app/mybook/repository/UserRepository.java
+++ src/main/java/com/app/mybook/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.app.mybook.repository;
 
-public interface UserRepository {
+import com.app.mybook.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
